88632b9fb5ffe3ba9ebc97589f77cd5a0fdcb665
Matthew Danish <mrd@debian.org>
2011-11-09 17:23:46 -0500

    signed types

--- include/types.h
+++ include/types.h
@@ -62,6 +62,11 @@ typedef unsigned short u16;
 typedef unsigned int u32;
 typedef unsigned long long u64;
 
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+typedef signed long long s64;
+
 typedef u32 physaddr;
 
 #endif

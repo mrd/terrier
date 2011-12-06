59aa2d222072f0a7b945dae25d89751ebb17d353
Matthew Danish <mrd@debian.org>
2011-12-06 16:20:12 -0500

    virtual to physical address translation function

--- include/mem/virtual.h
+++ include/mem/virtual.h
@@ -79,6 +79,7 @@ status vmm_activate_pagetable(pagetable_t *pt);
 status vmm_map_region(region_t *r);
 status vmm_init_pagetable(pagetable_t *pt);
 status vmm_map_region_find_vstart(region_t *r);
+status vmm_get_phys_addr(void *vaddr, physaddr *paddr);
 
 #endif
 

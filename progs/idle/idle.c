a2885ba43d351155cb1d674bf393c46f944fd4a8
Matthew Danish <mrd@debian.org>
2012-02-03 14:42:06 -0500

    change idle program to be register-heavy as a testbed

--- progs/idle/idle.c
+++ progs/idle/idle.c
@@ -1,8 +1,19 @@
-int test = 1;
-
 int main(void)
 {
-  asm volatile("SVC #123");
-  for(;;);
+  /* code that uses a lot of registers -- and gets messed up if registers are corrupted */
+  asm volatile("MOV r0, #1\n MOV r1, #11\n MOV r2, #13\n MOV r4, #0":::"r0","r1","r2","r4");
+  for(;;) {
+    asm volatile("SVC #1");
+    asm volatile("1: ADD r4, r4, #1\n"
+                 "   ADD r6, r1, r2\n"
+                 "   MOV r5, r0, LSL r6\n"
+                 "   CMP r5, r4\n"
+                 "   BNE 1b\n"
+                 "   MOV r0, #1\n"
+                 "   MOV r1, #11\n"
+                 "   MOV r2, #13\n"
+                 "   MOV r4, #0\n":::"r0","r1","r2","r3","r4","r5","r6");
+
+  }
   return 0;
 }

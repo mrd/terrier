7d555ba993497a3f60dabab56f7faec32dfaac58
Matthew Danish <mrd@debian.org>
2012-05-14 16:45:55 -0400

    this test is messing with its own registers

--- progs/idle/idle.c
+++ progs/idle/idle.c
@@ -8,7 +8,6 @@ int main(void)
   /* code that uses a lot of registers -- and gets messed up if registers are corrupted */
   asm volatile("MOV r0, #1\n MOV r1, #11\n MOV r2, #13\n MOV r4, #0":::"r0","r1","r2","r4");
   for(;;) {
-    entry();
     asm volatile("SVC #1");
     asm volatile("1: ADD r4, r4, #1\n"
                  "   ADD r6, r1, r2\n"

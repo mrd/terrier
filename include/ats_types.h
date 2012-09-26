e1f15ee6774750c73546d8a45cba523cbec41424
Matthew Danish <mrd@debian.org>
2012-09-26 14:05:39 -0400

    ats_int_type = s32

--- include/ats_types.h
+++ include/ats_types.h
@@ -3,7 +3,8 @@
 
 #include "types.h"
 
-typedef u32 ats_int_type, ats_bool_type, ats_uint_type;
+typedef u32 ats_bool_type, ats_uint_type;
+typedef s32 ats_int_type;
 typedef void ats_void_type;
 typedef void *ats_ptr_type, *ats_ref_type, *ats_clo_ptr_type, *ats_fun_ptr_type;
 

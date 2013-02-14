9610aa1e36b71dc44e2c5d97040081e0c3ba42a7
Matthew Danish <mrd@debian.org>
2013-02-14 15:42:41 -0500

    test the debug syscall

--- progs/uart/uart.c
+++ progs/uart/uart.c
@@ -1,8 +1,21 @@
+#include<user.h>
+
 unsigned int _scheduler_capacity = 3 << 14;
 unsigned int _scheduler_period = 12 << 14;
 unsigned int _scheduler_affinity = 1;
+
+static inline void svc3(char *ptr, u32 len, u32 noprefix)
+{
+  register char *r0 asm("r0") = ptr;
+  register u32 r1 asm("r1") = len;
+  register u32 r2 asm("r2") = noprefix;
+  asm volatile("SVC #3"::"r"(r0),"r"(r1),"r"(r2));
+}
+
 int main(void)
 {
+  svc3("Test\n", 6, 0);
+
   /* code that uses a lot of registers -- and gets messed up if registers are corrupted */
   asm volatile("MOV r0, #1\n MOV r1, #11\n MOV r2, #13\n MOV r4, #0":::"r0","r1","r2","r4");
   for(;;) {
